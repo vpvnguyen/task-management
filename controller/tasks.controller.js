2b20d98019c3412d9a180217b591086921ec494d
vpvnguyen <vpvnguyen@gmail.com>
2020-05-01 01:18:38 -0700

    create CRUD routes with express router

--- controller/tasks.controller.js
+++ controller/tasks.controller.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const tasksModel = require("../model/tasks.model");
-
+// get all tasks
 router.get("/getAllTasks", (req, res) => {
-  res.json("getAllTasks");
+  console.log("getAllTasks");
+  res.send(200);
+});
+
+// create a task
+router.post("/createTask", (req, res) => {
+  console.log("createTask");
+  res.send(200);
+});
+
+// edit a task by ID
+router.put("/editTask/:id", (req, res) => {
+  console.log("editTask");
+  console.log(req.params.id);
+  res.send(200);
+});
+
+// delete a task by ID
+router.delete("/deleteTask/:id", (req, res) => {
+  console.log("deleteTask");
+  console.log(req.params.id);
+  res.send(200);
 });
 
 module.exports = router;
