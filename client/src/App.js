e6f779fd6edfffd1234b23231015a0c0ae7a97ad
vpvnguyen <vpvnguyen@gmail.com>
2020-05-01 23:49:27 -0700

    imported task list and task input components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-
 import ListTasks from "./components/ListTasks";
+import InputTask from "./components/InputTask";
 
 function App() {
   return (
     <React.Fragment>
       <div className="container">
+        <InputTask />
         <ListTasks />
       </div>
     </React.Fragment>
