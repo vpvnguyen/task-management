63bf31e218b544c1161a0347c80bd549de5b4fea
vpvnguyen <vpvnguyen@gmail.com>
2020-05-01 22:24:20 -0700

    add sql script to seed mock data

--- config/database.sql
+++ config/database.sql
@@ -6,17 +6,16 @@ CREATE DATABASE task_management_system;
 -- create a table called tasks
 CREATE TABLE tasks(
     id SERIAL PRIMARY KEY,
-    category VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL
+    description VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL
 );
 
 -- insert mock data and display
-INSERT INTO tasks(category, description) 
+INSERT INTO tasks(description, category) 
 VALUES
-('Backlog', 'New item 1'),
-('New', 'New item 2'),
-('InProgress', 'New item 3'),
-('Complete', 'New item 4');
+('Create technical documentation', 'Documentation'),
+('Create GitHub Repo', 'Code'),
+('Build a simple app', 'Code');
 
 SELECT * FROM tasks;
 
